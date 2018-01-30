38d5ab1f8f6dd55bec45fbe3f26bed673fa16960
manishkumar212111 <manish.kumar212111@gmail.com>
2018-01-31 00:32:53 +0530

    signup file

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,10 +1,104 @@
-from src import app
-from flask import jsonify, request, render_template
+from flask import Flask,request
 import requests
+from flask import jsonify
 import json
+#from urllib3 import request
+app=Flask(__name__)
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    content = request.get_json(force=True)
+      # This is the url to which the query is made
+
+    js = json.loads(content)
+    b =check_password(js['password']);
+    if b == False :
+        list = [
+            {
+                "code": "error",
+                "message": "Entered password must be atleast 8 digit",
+                "detail": "null"
+            }
+        ]
+        return jsonify(resp=list)
+
+
+
+    # This is the url to which the query is made
+    url = "https://auth.octagon58.hasura-app.io/v1/signup"
+
+    # This is the json payload for the query
+    requestPayload = {
+    "provider": "username",
+    "data": {
+        "username": js['username'],
+        "password": js['password']
+    }
+    }
+
+    # Setting headers
+    headers = {
+    "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+    resp1=resp
+    data=resp.json()
+    if data['code'] == "user-exists":
+        print "user exists"
+    else:
+        # This is the url to which the query is made
+        url = "https://data.octagon58.hasura-app.io/v1/query"
+
+        # This is the json payload for the query
+        requestPayload = {
+            "type": "insert",
+            "args": {
+                "table": "signup",
+                "objects": [
+                    {
+                        "uid": data['code'] ,
+                        "email": js['email'],
+                        "mobile": js['mobile'],
+                        "currency":js['currency']
+                    }
+                ]
+            }
+        }
+
+        # Setting headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer b660de1696fbdc8daa1d32d1d8f19bf03315ec407b9e2ebf"
+        }
+
+        # Make the query and store response in resp
+        resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+        # resp.content contains the json response.
+        #print resp.content
+
+
+    return resp1.content
+
+
+def check_password(str):
+    a=len(str)
+    if a<8:
+        return False
+    else:
+        return True
+    return True
+
+
+
 
 
 
 @app.route("/")
 def hello():
   return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
