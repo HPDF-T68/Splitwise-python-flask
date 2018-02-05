8c15d3bc37e798f36d2c1397a8320ea6396fc70f
manishkumar212111 <manish.kumar212111@gmail.com>
2018-02-05 20:24:58 +0530

    test

--- microservices/app/conf/gunicorn_config.py
+++ microservices/app/conf/gunicorn_config.py
@@ -4,10 +4,10 @@ bind = "0.0.0.0:8080"
 # set number of workers based on CPU - good for production
 # workers = 1
 # for dev, set worker = 1
-workers = 1
+workers = 3
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = "debug"
 capture_output = True
 enable_stdio_inheritance = True
- TIMEOUT=120
+TIMEOUT=120
