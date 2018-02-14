4bc6acf50f3d57611e4e619a1d77b04235ba98c9
manishkumar212111 <manish.kumar212111@gmail.com>
2018-02-14 23:11:38 +0530

    test

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -192,7 +192,7 @@ def update_email():
 @app.route('/change_email',methods=['POST','GET'])
 def change_email():
     if request.method =='POST':
-        email=request.form['form']
+        email=request.form['email']
 
         url = "https://data.octagon58.hasura-app.io/v1/query"
 
