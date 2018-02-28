635e77a5a138c2eb1a5c74c6811e00b00be826a1
manishkumar212111 <manish.kumar212111@gmail.com>
2018-03-01 00:27:04 +0530

    test

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -720,8 +720,7 @@ def more_detail():
 
     # Setting headers
     headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer c6fd65b8291402d919b7e940069cdd655109daa75b970967"
+        "Content-Type": "application/json"
     }
 
     # Make the query and store response in resp
@@ -830,8 +829,7 @@ def more_detail():
 
     # Setting headers
     headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer c6fd65b8291402d919b7e940069cdd655109daa75b970967"
+        "Content-Type": "application/json"
     }
 
     # Make the query and store response in resp
